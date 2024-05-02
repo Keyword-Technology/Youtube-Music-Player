c76fcefd9626cf8543a77217967b87bfaf82bd19
Connor <hessconnor41@gmail.com>
2024-05-02 09:38:20 +0200

    added downloading

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import configparser
 import youtube_dl
 
 
-# Import the configparser module
 
 # Create a new instance of the ConfigParser class
 config = configparser.ConfigParser()
@@ -10,7 +9,10 @@ config = configparser.ConfigParser()
 if not config.read('config.ini'):
     # Add sections and options to the config file
     config['General'] = {
-        'playlist link': 'https://'
+        'playlist link': 'https://',
+        'output folder': '.',
+        'refresh rate': '3600',
+        'download': 'True'
     }
 
     # Write the config file to disk
@@ -20,14 +22,14 @@ if not config.read('config.ini'):
 
 
 # Define the YouTube playlist URL
-playlist_url = ""
+playlist_url = config['General']['playlist link']
 
 # Create a YouTubeDL object
 ydl = youtube_dl.YoutubeDL({})
 
 # Extract the playlist information
 with ydl:
-    playlist_info = ydl.extract_info(playlist_url, download=False)
+    playlist_info = ydl.extract_info(playlist_url, download=(config['General']['download'] == 'True'))
 
 # Iterate over each video in the playlist
 for video in playlist_info["entries"]:
